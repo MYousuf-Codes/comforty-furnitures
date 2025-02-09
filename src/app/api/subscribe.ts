a864a55c20cae4c1cba68402c3db94385fef54db
MYousuf-Codes <imhere4uinfo@gmail.com>
2025-02-10 00:17:51 +0500

    FormCheckout and subscribe.ts updated

--- src/app/api/subscribe.ts
+++ src/app/api/subscribe.ts
@@ -1,10 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import Resend from 'resend';
+import { NextApiRequest, NextApiResponse } from "next";
+import Resend from "resend";
 
 const resend = new (Resend as any)(process.env.RESEND_API_KEY);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const { email } = req.body;
 
     const EMAIL_FROM = process.env.EMAIL_USER;
@@ -14,21 +14,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const response = await resend.emails.send({
         from: EMAIL_FROM,
         to: EMAIL_TO,
-        subject: 'New Subscription',
+        subject: "New Subscription",
         html: `<p>New subscription with email: ${email}</p>`,
       });
 
       if (response?.id) {
         return res.status(200).json({ success: true });
       } else {
-        return res.status(400).json({ success: false, error: 'Failed to send email' });
+        return res
+          .status(400)
+          .json({ success: false, error: "Failed to send email" });
       }
     } catch (error) {
-      console.error('Error sending email:', error);
-      return res.status(500).json({ success: false, error: 'Internal Server Error' });
+      console.error("Error sending email:", error);
+      return res
+        .status(500)
+        .json({ success: false, error: "Internal Server Error" });
     }
   } else {
-    return res.status(405).json({ success: false, error: 'Method Not Allowed' });
+    return res
+      .status(405)
+      .json({ success: false, error: "Method Not Allowed" });
   }
 };
 
